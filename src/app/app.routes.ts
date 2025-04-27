4216b8ae645fe805c425b0f7180f7d7d09319694
Pablo Medina <1amedinapablo@gmail.com>
2025-04-27 20:37:40 +0200

    feat: adjusted main route configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,51 +4,34 @@ export const routes: Routes = [
   {
     path: '',
     redirectTo: 'recipes',
-    pathMatch: 'full',
+    pathMatch: 'full'
   },
   {
     path: 'recipes',
     children: [
       {
         path: '',
-        loadComponent: () =>
-          import('./recipes/recipes.page').then((m) => m.RecipesPage),
+        loadComponent: () => import('./recipes/recipes.page').then((m) => m.RecipesPage)
       },
       {
         path: ':recipeId',
         loadComponent: () =>
           import('./recipes/recipe-detail/recipe-detail.page').then(
             (m) => m.RecipeDetailPage
-          ),
-      },
-    ],
+          )
+      }
+    ]
   },
   {
     path: 'auth',
-    loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
-  },
-  {
-    path: 'discover',
-    loadComponent: () => import('./places/discover/discover.page').then( m => m.DiscoverPage)
-  },
-  {
-    path: 'new-offer',
-    loadComponent: () => import('./places/offers/new-offer/new-offer.page').then( m => m.NewOfferPage)
-  },
-  {
-    path: 'edit-offer',
-    loadComponent: () => import('./places/offers/edit-offer/edit-offer.page').then( m => m.EditOfferPage)
-  },
-  {
-    path: 'place-detail',
-    loadComponent: () => import('./places/discover/place-detail/place-detail.page').then( m => m.PlaceDetailPage)
-  },
-  {
-    path: 'offer-bookings',
-    loadComponent: () => import('./places/offers/offer-bookings/offer-bookings.page').then( m => m.OfferBookingsPage)
+    loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage)
   },
   {
     path: 'places',
-    loadComponent: () => import('./places/places.page').then( m => m.PlacesPage)
+    loadComponent: () => import('./places/places.page').then((m) => m.PlacesPage)
   },
+  {
+    path: 'bookings',
+    loadComponent: () => import('./bookings/bookings.page').then((m) => m.BookingsPage)
+  }
 ];
